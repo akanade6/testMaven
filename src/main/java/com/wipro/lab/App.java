b59a2b53a74fec4284a5ae7c7a42822a5fe3b184
Kanade <AbhijeetArvind.Kanade@standardbank.co.za>
2017-05-08 15:27:23 +0200

    Test first commit

---
+++ src/main/java/com/wipro/lab/App.java
@@ -0,0 +1,17 @@
+package com.wipro.lab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public int add(int num1, int num2){
+    	
+    	return num1+num2;
+    }
+}
---
+++ src/test/java/com/wipro/lab/AppTest.java
@@ -0,0 +1,11 @@
+package com.wipro.lab;
+
+import junit.framework.TestCase;
+
+public class AppTest extends TestCase {
+
+	public void testAdd() {
+		assertEquals(5, new App().add(3,2));
+	} 
+
+}
